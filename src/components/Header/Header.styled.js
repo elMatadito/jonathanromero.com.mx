629d6523d746fada922b92ac47f7a23b91d39c96
Jonathan Romero <jromeroh93@gmail.com>
2023-10-14 20:48:37 -0600

    Improve header design and background

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,14 +1,51 @@
 import styled from 'styled-components';
 
+export const HeaderTop = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin: 10px 0px;
+
+  .menu{
+    svg{
+      width: 30px;
+      height: 30px;
+    }
+  }
+
+  .social{
+    text-align: center;
+
+    a{
+      svg{
+        width: 30px;
+        height: 30px;
+        margin-right: 10px;
+        filter: grayscale(1);
+
+        &:hover{
+          filter: grayscale(0);
+        }
+      }
+    }
+  }
+`;
+
 export const HeaderContainer = styled.header`
-  padding-top: 50px;
   display: grid;
   grid-template-columns: 1fr;
   align-items: center;
   margin-bottom: 35px;
+  background: #1e2738;
+  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(148, 163, 184, 0.1) 0px 1px 0px 0px inset;
+  padding: 14px;
+  border-radius: 8px;
 
   .info{
     grid-row: 2;
+
+    h1{
+      color: #67E6B0;
+    }
   }
 
   img{
--- src/components/Jobs/Jobs.styled.js
+++ src/components/Jobs/Jobs.styled.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Job = styled.div`
   padding: 14px;
   border-radius: 8px;
-  background: rgba(30,41,59,.5);
+  // background: rgba(30,41,59,.5);
+  background: #1e2738;
   margin-bottom: 10px;
   box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(148, 163, 184, 0.1) 0px 1px 0px 0px inset;
   
